8b56c3ab2020da27a05729d35f125b8b2c3385ba
hugofolloni <hugofollogua07@gmail.com>
2024-05-26 16:17:48 -0300

    changing database connection string

--- server/api/config.js
+++ server/api/config.js
@@ -15,7 +15,7 @@ const mySqlconfig = {
     }
 }
 
-const pgConfig = {pool: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`}
+const pgConfig = {pool: process.env.DB_URI}
 
 console.log(pgConfig)
 module.exports = pgConfig;
